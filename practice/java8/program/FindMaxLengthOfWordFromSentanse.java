de6c0a984cc71feb3e1db46719aabd447e255d98
pavan_gawande <pavan_gawande@epam.com>
2024-08-13 17:06:42 +0530

    method reference added

--- practice/java8/program/FindMaxLengthOfWordFromSentanse.java
+++ practice/java8/program/FindMaxLengthOfWordFromSentanse.java
@@ -8,7 +8,7 @@ public class FindMaxLengthOfWordFromSentanse {
     public static void main(String[] args) {
         String string = "I am interested123455 to grow in my organization";
 
-        String maxLength = Arrays.asList(string.split(" ")).stream().max(Comparator.comparing(s -> s.length())).get();
+        String maxLength = Arrays.stream(string.split(" ")).max(Comparator.comparing(String::length)).get();
 
         System.out.println(maxLength);
     }
