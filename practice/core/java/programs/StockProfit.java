286fb9c032e7d45ff1233353c62101978a4ec593
pavan_gawande <pavan_gawande@epam.com>
2024-08-19 19:29:42 +0530

    Given an array prices[] of size N denoting the cost of stock on each day, the task is to find the maximum total profit if we can buy and sell the stocks any number of times.

---
+++ practice/core/java/programs/StockProfit.java
@@ -0,0 +1,18 @@
+package practice.core.java.programs;
+
+/**
+ * Given an array prices[] of size N denoting the cost of stock on each day, the task is to find the maximum total profit if we can buy and sell the stocks any number of times.
+ */
+public class StockProfit {
+    public static void main(String[] args) {
+        int[] prices = {100, 180, 260, 310, 40, 535, 695};
+        Integer totalProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                totalProfit += prices[i] - prices[i - 1];
+            }
+        }
+
+        System.out.println("Max Profit "+totalProfit);
+    }
+}
