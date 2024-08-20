2848c8fbd201d4f794413910a7bff596aa476981
pavan_gawande <pavan_gawande@epam.com>
2024-08-20 11:46:58 +0530

    Builder design pattern

---
+++ practice/design/pattern/builder/BuilderMain.java
@@ -0,0 +1,19 @@
+package practice.design.pattern.builder;
+
+/**
+ * By using the Builder Pattern,
+ * you can avoid issues related to constructor overloading,
+ * improve code readability, and achieve better encapsulation and immutability.
+ */
+public class BuilderMain {
+
+    public static void main(String[] args) {
+        // Create a Product using the Builder
+        Product product = new Product.Builder()
+                .setPart1("Part1")
+                .setPart2("Part2")
+                .build();
+
+        System.out.println(product);
+    }
+}
