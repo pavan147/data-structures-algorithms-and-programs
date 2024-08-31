3e25d8cfd59b208648dd587685dd3f010f5a79aa
pavan_gawande <pavan_gawande@epam.com>
2024-08-31 14:40:19 +0530

    Find subarray

---
+++ practice/java/dsa/FindSubArrayIsEqualToSum.java
@@ -0,0 +1,29 @@
+package practice.java.dsa;
+
+
+import java.util.HashMap;
+
+
+public class FindSubArrayIsEqualToSum {
+
+    public static void main(String[] args) {
+        int sum = 7;
+        int[] arr = {1, 1, 2, 3, 5, 8, 13};
+        HashMap<Integer, Integer> hashMap = new HashMap();
+
+        int currentSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            currentSum += arr[i];
+            if (currentSum - sum == 0) {
+                System.out.println(0 + " " + i);
+            }
+
+            if (hashMap.containsKey(currentSum - sum)) {
+
+                System.out.println(hashMap.get(currentSum - sum) + " -" + i);
+            }
+            hashMap.put(currentSum, i);
+        }
+
+    }
+}
