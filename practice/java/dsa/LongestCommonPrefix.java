76505d7a7d9157c48349fa108616a2d64d9a0443
pavan_gawande <pavan_gawande@epam.com>
2024-08-31 14:41:37 +0530

    longest common prefix

---
+++ practice/java/dsa/LongestCommonPrefix.java
@@ -0,0 +1,37 @@
+package practice.java.dsa;
+
+/**
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ *
+ * If there is no common prefix, return an empty string "".
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: strs = ["flower","flow","flight"]
+ * Output: "fl"
+ * Example 2:
+ *
+ * Input: strs = ["dog","racecar","car"]
+ * Output: ""
+ * Explanation: There is no common prefix among the input strings.
+ */
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        String[] strs = {"flower", "flow", "flight"};
+
+        System.out.println(longestCommonPrefix(strs));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+
+            while (strs[i].indexOf(prefix) != 0){
+                prefix = prefix.substring(0,prefix.length() -1);
+            }
+        }
+        return prefix;
+    }
+}
