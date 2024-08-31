229ad293f5cc8d2cf8f4d17d376547e6c50c3d69
pavan_gawande <pavan_gawande@epam.com>
2024-08-31 14:45:09 +0530

    consitive ofdd number

---
+++ practice/java/dsa/ThreeConsitiveOddNum.java
@@ -0,0 +1,42 @@
+package practice.java.dsa;
+
+/*
+Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
+
+
+Example 1:
+
+Input: arr = [2,6,4,1]
+Output: false
+Explanation: There are no three consecutive odds.
+Example 2:
+
+Input: arr = [1,2,34,3,4,5,7,23,12]
+Output: true
+Explanation: [5,7,23] are three consecutive odds.
+ */
+public class ThreeConsitiveOddNum {
+    public static void main(String[] args) {
+        int[] arr = {424, 915, 193, 591, 923};
+        System.out.println(threeConsecutiveOdds(arr));
+    }
+
+    public static boolean threeConsecutiveOdds(int[] arr) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            boolean numberIsOdd = arr[i] % 2 != 0;
+            if (count == 3) {
+                break;
+            }
+            if (numberIsOdd) {
+                count++;
+            } else {
+
+                count = 0;
+
+            }
+        }
+
+        return count == 3;
+    }
+}
