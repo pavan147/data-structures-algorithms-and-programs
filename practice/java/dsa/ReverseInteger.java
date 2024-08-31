e680f53bfe52f7108d00c370d2d6a73cef8ff151
pavan_gawande <pavan_gawande@epam.com>
2024-08-31 14:45:51 +0530

    Reverse Integer

---
+++ practice/java/dsa/ReverseInteger.java
@@ -0,0 +1,42 @@
+package practice.java.dsa;
+
+/**
+ * Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+ *
+ * Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: x = 123
+ * Output: 321
+ * Example 2:
+ *
+ * Input: x = -123
+ * Output: -321
+ * Example 3:
+ *
+ * Input: x = 120
+ * Output: 21
+ *
+ */
+public class ReverseInteger {
+    public static void main(String[] args) {
+        int x = 1534236469;
+        System.out.println(reverse(x));
+    }
+
+    public static int reverse(int x) {
+        int reverse = 0;
+        int digit = 0;
+
+        while (x != 0) {
+            digit = x % 10;
+            x = x / 10;
+            reverse = reverse * 10 + digit;
+        }
+
+      return reverse;
+    }
+}
