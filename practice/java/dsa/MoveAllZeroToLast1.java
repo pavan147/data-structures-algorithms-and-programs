b8c3abb9d8e8aa728b19cdc10506c6305c22d817
pavan_gawande <pavan_gawande@epam.com>
2024-08-31 14:43:19 +0530

    Move 0 to last of array

---
+++ practice/java/dsa/MoveAllZeroToLast1.java
@@ -0,0 +1,37 @@
+package practice.java.dsa;
+
+import java.util.ArrayList;
+
+/*
+write a code to shift 0’s in an array of integers to the end of the array:       input - 1,4,6,0,2,0,8
+output- 1,4,6,2,8,0,0
+ */
+public class MoveAllZeroToLast1 {
+    public static void main(String[] args) {
+
+        int[] array = {1, 4, 6, 0, 2, 0, 8};
+        ArrayList<Integer> zeroIndexList = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
+                zeroIndexList.add(i);
+            }
+        }
+        int incrementList = 0;
+        for (int i = array.length - zeroIndexList.size(); i < array.length; i++) {
+
+            if (array[i] != 0) {
+                int temp = array[i];
+                array[i] = array[zeroIndexList.get(incrementList)];
+                array[zeroIndexList.get(incrementList)] = temp;
+
+                incrementList++;
+            }
+
+
+        }
+        for (int i = 0; i < array.length; i++) {
+
+            System.out.print(array[i] + ", ");
+        }
+    }
+}
